605869c563d8b5e274ee60340e5d07ad54593acd
Dav HOJT <davhojt@gmail.com>
2018-05-06 16:03:28 +0200

    Updated to resolve double free.

--- the_other_one/checker/srcs/sort_test.c
+++ the_other_one/checker/srcs/sort_test.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/06 04:47:02 by dhojt             #+#    #+#             */
-/*   Updated: 2018/05/06 05:59:42 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/05/06 07:53:42 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 void	sort_test(t_frame *frame)
 {
 	t_stack		*tmp;
+	int			error;
 
 	tmp = frame->a;
+	error = (frame->b) ? 1 : 0;
 	while (tmp->next != frame->a)
 	{
 		if (tmp->num > tmp->next->num)
-		{
-			write(1, "KO\n", 3);
-			exit(-1);
-		}
+			error = 1;
 		tmp = tmp->next;
 	}
-	write(1, "OK\n", 3);
+	if (error)
+		write(1, "KO\n", 3);
+	else
+		write(1, "OK\n", 3);
 }
